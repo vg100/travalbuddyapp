7f51bb6105576eb6f52853159aad137ecdb1ae4b
Vijay_Gupta <40712807+vg100@users.noreply.github.com>
2020-09-20 18:04:02 +0530

    Add files via upload

---
+++ src/Icons.js
@@ -0,0 +1,8 @@
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Octicons from "react-native-vector-icons/Octicons";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Feather from "react-native-vector-icons/Feather";
+import Ionicons from "react-native-vector-icons/Ionicons";
+export {MaterialCommunityIcons,Fontisto,AntDesign,Octicons,MaterialIcons,Feather,Ionicons}
